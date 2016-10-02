7e7ee4c568d22c60da25700b697acce2ab905225
Aaron Karras <chw111111@hotmail.com>
2016-10-01 21:39:26 -0600

    Renamed Macros

---
+++ MysteryCrateEditor/MysteryCrateEditor/Libraries/MysteryCrate/Rewards/Macros.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MysteryCrateEditor.Libraries.MysteryCrate.Rewards
+{
+    public class TagMacro
+    {
+        public List<IRewardTag> Rewards { get; set; }
+        public Dictionary<string,string> MacroValues { get; set; }
+    }
+    public class RewardMacro
+    {
+        public List<Reward> Rewards { get; set; }
+        /// <summary>
+        /// Macro Values, key is text to be replaced, value is text to replace the key with.
+        /// </summary>
+        public Dictionary<string,string> MacroValues { get; set; }
+
+        public void doReplacement()
+        {
+            foreach(var reward in Rewards)
+            {
+                
+            }
+            foreach(var macroValue in MacroValues)
+            {
+                
+            }
+        }
+    }
+    public interface IMacro
+    {
+        
+    }
+
+}
